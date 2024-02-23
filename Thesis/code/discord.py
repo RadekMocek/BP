86d58787222f2dcd3cdc1235593f771d1abaea79
RadekMocek <radek.mocek@tul.cz>
2024-02-24 00:33:38 +0100

    (thesis wip 14)

---
+++ Thesis/code/discord.py
@@ -0,0 +1,13 @@
+from discord import app_commands
+# ...
+@app_commands.command()
+async def pow(self,
+              itx: discord.Interaction,
+              num: int,
+              exp: int) -> None:
+    """Umocní číslo 'num' na 'exp'."""
+    view = discord.ui.View()
+    view.add_item(discord.ui.Button(label="Example",
+                                    url="https://example.com"))
+    await itx.response.send_message(content=num ** exp,
+                                    view=view)
