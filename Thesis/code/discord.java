2bcbba10555b2b54ac9dabf619efc309b832d131
RadekMocek <radek.mocek@tul.cz>
2024-04-21 22:48:53 +0200

    (thesis wip 37)

--- Thesis/code/discord.java
+++ Thesis/code/discord.java
@@ -6,6 +6,6 @@ public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
     if (event.getName().equals("pow")) {
         int num = event.getOption("num").getAsInt();
         int exp = event.getOption("exp").getAsInt();
-        event.reply("" + Math.pow(num,exp)).addActionRow(Button.link("https://example.com", "Example")).queue();
+        event.reply("" + Math.pow(num, exp)).addActionRow(Button.link("https://example.com", "Example")).queue();
     }
 }
